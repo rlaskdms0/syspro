b47f14fffb0e9f44a20be29e441899b22d512e32
kimnaeun <kne050726@cu.ac.kr>
2025-10-14 02:10:27 +0000

    chap6

---
+++ chap6/prob2/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+
+int main(int argc, char* argv[]){
+	long strtol();
+	int newmode;
+	newmode=(int)strtol(argv[1], (char **)NULL, 8);
+	if(chmod(argv[2],newmode)==-1){
+		perror(argv[2]);
+		exit(1);
+	}
+	exit(0);
+}
+
